2afb31af2ac994d03a27bd84f1eb17d0555247c9
fikri <fuadfikri01@gmail.com>
2022-01-12 21:30:12 +0700

    add dependency and configuration

--- src/main/java/id/fikri/syamsudin/microservices/config/configserver/ConfigServerApplication.java
+++ src/main/java/id/fikri/syamsudin/microservices/config/configserver/ConfigServerApplication.java
@@ -2,21 +2,23 @@ package id.fikri.syamsudin.microservices.config.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 
 
-	@RequestMapping("/")
-	public String home() {
-		return "Hello world";
-	}
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
 }
