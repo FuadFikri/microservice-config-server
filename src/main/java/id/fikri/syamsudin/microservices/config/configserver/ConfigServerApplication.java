60f2ff5edb29231e7f57f93c125034867a15ce44
fikri <fuadfikri01@gmail.com>
2022-01-12 21:00:20 +0700

    initial commit

---
+++ src/main/java/id/fikri/syamsudin/microservices/config/configserver/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package id.fikri.syamsudin.microservices.config.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello world";
+	}
+}
